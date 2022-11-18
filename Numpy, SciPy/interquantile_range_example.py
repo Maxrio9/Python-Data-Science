9de038f52734740ad45704641af2460d3663c66f
Maxrio <lasse@rosendahl-energietechnik.de>
2022-11-19 00:42:30 +0100

    added code for interquantile range

---
+++ Numpy, SciPy/interquantile_range_example.py
@@ -0,0 +1,9 @@
+from scipy.stats import iqr
+
+dataset = [-50, -24, -13, -2, 0, 12, 15, 18, 73, 90, 10000]
+
+dataset_range = max(dataset) - min(dataset)
+dataset_iqr = iqr(dataset)
+
+print("The range of the dataset is "+str(dataset_range))
+print("The IQR of the dataset is "+str(dataset_iqr))
